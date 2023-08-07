36175611b3d3edc9e78a82119f4a18d61a5c9235
Quynh Nguyen <qtn2@case.edu>
2023-08-07 03:25:05 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css'
+import Navbar from './components/Navbar.jsx'
+import Info from './components/Info'
+import Card from './components/Card'
+import '@fontsource/poppins';
+import HostData from './data/HostData'
+function App() {
+  const hostData = HostData.map (data => {
+    return <Card 
+          key = {data.id}
+          {...data}
+          />
+  })
+  return (
+    <div >
+        <Navbar />
+        <Info />
+      <section className = "cards-list"> 
+        {hostData}
+      </section>
+    </div>
+
+  )
+}
+
+export default App
+
